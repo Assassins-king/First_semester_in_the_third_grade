79a80b85148f89bfdbba20b7c74f92d29d21d1ea
Assassins-king <1031563986@qq.com>
2022-11-04 11:50:20 +0800

    20221104

--- vue项目/vue_cli_learn/34_src_路由的params参数/main.js
+++ vue项目/vue_cli_learn/34_src_路由的params参数/main.js
@@ -2,15 +2,16 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-//引入插件
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
 
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-import VueRouter from "vue-router";
-//引入路由器
-import router from "./router";
-
+//应用插件
 Vue.use(VueRouter)
+
 //创建vm
 new Vue({
 	el:'#app',
