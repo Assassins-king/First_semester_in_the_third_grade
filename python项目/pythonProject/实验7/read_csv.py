9a1b4a1955e54054e6aba0b8563b27cc3241d269
Assassins-king <1031563986@qq.com>
2022-12-22 23:22:43 +0800

    20221222

--- python项目/pythonProject/实验7/read_csv.py
+++ python项目/pythonProject/实验7/read_csv.py
@@ -19,7 +19,30 @@ with open('usercopy.csv', 'w', encoding='utf8') as file:
     write = csv.writer(file)
     write.writerows(csv_list)
 
+import csv
 
+# 打开yq.csv文件，设置写入模式
+with open('yq.csv', 'w', newline='') as csvfile:
+    # 创建csv写入器
+    writer = csv.writer(csvfile)
 
+    # 循环遍历数据列表
+    for data in data_list:
+        # 使用writerow()函数将数据写入到yq.csv文件中
+        writer.writerow(data)
 
+import csv
+
+# 创建一个空列表，用于存储读取的数据
+data_list = []
+
+# 打开yq.csv文件，设置读取模式
+with open('yq.csv', 'r') as csvfile:
+    # 创建csv读取器
+    reader = csv.reader(csvfile)
+
+    # 循环遍历读取器
+    for row in reader:
+        # 将读取到的每一行数据添加到列表中
+        data_list.append(row)
 
--- python项目/pythonProject/测试/oppp.py
+++ python项目/pythonProject/测试/oppp.py
@@ -2,4 +2,43 @@
 # @Time    : 2022/12/7 15:05
 # @Author  : 崔文帅
 # @File    : oppp.py
-print(type((1,2,3,4)))
+# 引入必要的库
+import csv
+
+# 创建一个空字典，用于存储省份名称和新增确诊数量的对应关系
+province_confirmed_dict = {}
+
+# 创建一个空字典，用于存储省份名称和新增无症状数量的对应关系
+province_symptomless_dict = {}
+
+# 累计确诊的总数
+total_confirmed = 0
+
+# 打开yq.csv文件，设置读取模式
+with open('yq.csv', 'r') as csvfile:
+    # 创建csv读取器
+    reader = csv.reader(csvfile)
+
+    # 跳过标题行
+    next(reader)
+
+    # 循环遍历读取器
+    for row in reader:
+        # 获取省份名称、新增确诊数量和新增无症状数量
+        province, confirmed, symptomless = row[0], row[1], row[2]
+
+        # 将新增确诊数量转换为整数
+        confirmed = int(confirmed)
+
+        # 将新增无症状数量转换为整数
+        symptomless = int(symptomless)
+
+        # 统计省份名称和新增确诊数量的对应关系
+        if province in province_confirmed_dict:
+            province_confirmed_dict[province] += confirmed
+        else:
+            province_confirmed_dict[province] = confirmed
+
+        # 统计省份名称和新增无症状数量的对应关系
+        if province in province_symptomless_dict:
+            province_sym
